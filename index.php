be9d3e0140bf3e008434b92f14aa6af704848373
samjoosten <31620260+samjoosten@users.noreply.github.com>
2019-03-25 09:22:53 +0100

    Add files via upload
    initial commit

---
+++ index.php
@@ -0,0 +1,363 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Sam Joosten</title>
+    <link rel="icon" href="images/favicon.ico"/>
+    <!-- Mesta Tags -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="keywords" content="Succinct Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template,
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
+	<script type="application/x-javascript">
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<!-- //Meta Tags -->
+	<!-- Style Sheets -->
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+	<!--// Bootstrap-CSS -->
+	<link href="css/font-awesome.css" rel="stylesheet">
+	<!--// Font-Awesome-CSS -->
+	<link href="css/easy-responsive-tabs.css" rel='stylesheet' type='text/css' />
+	<!--// Responsive tabs -->
+	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+	<!-- //Style Sheets -->
+	<!-- web-fonts -->
+	<link href="//fonts.googleapis.com/css?family=Cinzel+Decorative:400,700,900" rel="stylesheet">
+	<link href="//fonts.googleapis.com/css?family=Open+Sans:300,400,600" rel="stylesheet">
+	<!--// web-fonts -->
+</head>
+
+<body>
+	<!--banner-->
+	<div class="banner" id="home">
+		<div class="container">
+			<!-- header -->
+			<div class="header">
+                <img src="images/Logo_v2.png" href="index.php" style="opacity: 0.7; position: absolute; width: 180px; height: 130px; top: 10%; margin-left: 20px">
+				<nav class="navbar navbar-default">
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+					</div>
+
+					<!-- navbar-header -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="index.php" class="hvr-underline-from-center active">Overzicht</a></li>
+							<li><a href="#about" class="hvr-underline-from-center scroll">Over mij</a></li>
+							<li><a href="#works" class="hvr-underline-from-center scroll">Werk</a></li>
+						</ul>
+					</div>
+					<div class="clearfix"> </div>
+				</nav>
+			</div>
+			<!-- //header -->
+
+			<!-- banner-text-w3layouts -->
+			<div class="banner-text-w3layouts">
+				<h3 style="font-family: 'Open Sans', sans-serif">Hallo, ik ben</h3>
+                <h2><span>S</span>am <span>J</span>oosten</h2>
+				<a target="_blank" href="/web/Files/Sam Joosten - CV_24032019.pdf" download="Sam Joosten - CV_24032019.pdf">Download CV</a>
+			</div>
+		</div>
+
+
+
+		<!--banner-Slider-->
+	</div>
+	<!--//banner-->
+	<div class="about-section" id="about">
+		<div class="container">
+			<h4 class="title-top-w3">In het kort<span>|</span></h4>
+			<h5 class="title-main-w3ls">Over Mij</h5>
+			<!--responsive-tabs-->
+			<div id="horizontalTab">
+
+				<ul class="resp-tabs-list">
+					<li>Over Mij</li>
+					<li>Educatie</li>
+					<li>Ervaringen</li>
+				</ul>
+				<div class="resp-tabs-container">
+
+					<div class="tab1">
+						<div class="col-md-10 about-left-w3layouts">
+							<ul>
+                                <p>Ik ben een gedreven developper die niet wegdeinst voor een uitdaging. Ik verdiep me graag in
+                                nieuwe technieken en ik ontwikkel mijzelf graag verder. Stuur mij gerust een bericht of neem een kijkje
+                                op mijn LinkedIn en Github!</p>
+                                <a href="mailto:sam.joosten90@gmail.com">sam.joosten90@gmail.com</a>
+                                <a href="https://github.com/samjoosten" class="fa fa-github" style="margin-left: 60%" aria-hidden="true"></a>
+                                <a href="https://www.linkedin.com/in/sam-joosten-230101160/" class="fa fa-linkedin" style="margin-left: 1%" aria-hidden="true"></a>
+							</ul>
+							<div class="pos-grid-agileinfo">
+								<img src="images/sam.jpeg" class="img-responsive" alt="">
+								<h5>Over Mij</h5>
+							</div>
+						</div>
+					</div>
+
+
+					<div class="tab3">
+						<div class="col-md-10 about-left-w3layouts">
+							<div class="about-info-w3-agileits">
+								<h6>HBO-ICT<span>2017-heden</span></h6>
+								<div class="clearfix"> </div>
+								<p>In september 2017 ben ik begonnen met de studie HBO-ICT, Software Engineering om precies te zijn aan de
+                                    Hogeschool van Amsterdam.</p>
+							</div>
+							<div class="about-info-w3-agileits mid-bar-agileits-w3layouts">
+								<h6>Havo diploma<span>2012-2016</span></h6>
+								<div class="clearfix"> </div>
+								<p>In 2012 is mijn middelbare schooltijd begonnen op het Erfgooiers College waar ik uiteindelijk in 2016
+                                    mijn Havo diploma bemachtigde.</p>
+							</div>
+							<div class="pos-grid-agileinfo">
+								<span class="fa fa-graduation-cap" aria-hidden="true"></span>
+								<h5>Educatie</h5>
+							</div>
+						</div>
+					</div>
+
+					<div class="tab4">
+						<div class="col-md-10 about-left-w3layouts">
+							<div class="about-info-w3-agileits">
+								<h6>Corendon lost & found (School project)<span>2017-2018</span></h6>
+								<div class="clearfix"> </div>
+								<p>Mede ontwikkelaar van de software voor een Java applicatie voor Corendon.</p>
+							</div>
+							<div class="about-info-w3-agileits mid-bar-agileits-w3layouts">
+								<h6>Detective Crawler (School project)<span>2018-2018</span></h6>
+								<div class="clearfix"> </div>
+								<p>Mede ontwikkelaar van een website en database voor een hybrid-boardgame.</p>
+							</div>
+                            <div class="about-info-w3-agileits mid-bar-agileits-w3layouts">
+                                <h6>Vitia<span>2018-2019</span></h6>
+                                <div class="clearfix"> </div>
+                                <p>Mede ontwikkelaar van een webapp waar surveys kunnen worden gehouden en visualisaties kunnen
+                                    worden gemaakt</p>
+                            </div>
+                            <div class="about-info-w3-agileits mid-bar-agileits-w3layouts">
+                                <h6>Dashboards Farmaceuten<span>2018-heden</span></h6>
+                                <div class="clearfix"> </div>
+                                <p>Mede ontwikkelaar van dashboards voor grote farma bedrijven, nationaal en internationaal</p>
+                            </div>
+							<div class="pos-grid-agileinfo">
+								<span class="fa fa-shield" aria-hidden="true"></span>
+								<h5>Ervaringen</h5>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Work -->
+	<div class="works-section" id="works">
+		<div class="container">
+			<h4 class="title-top-w3">Werk<span>|</span></h4>
+			<h5 class="title-main-w3ls">Opdrachten</h5>
+			<div class="col-md-7 work-left">
+				<div class="work-imgl zoom-img">
+				</div>
+				<div class="clearfix"> </div>
+				<div class="work2-images">
+					<div class="col-md-6 col-sm-6 col-xs-6 work1">
+						<a href="#" data-toggle="modal" data-target="#myModal3">
+							<img src="images/w3.jpg" alt=" " class="img-responsive">
+							</a>
+					</div>
+					<div class="col-md-6 col-sm-6 col-xs-6 work2">
+						<a href="#" data-toggle="modal" data-target="#myModal4">
+							<img src="images/w4.jpg" alt=" " class="img-responsive">
+						</a>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<div class="col-md-5  work-right">
+				<div class="work-imgr">
+				</div>
+				<div class="work-info">
+					<h4>Mijn werk</h4>
+					<p>Klik op de onderstaande knop om mijn werk te bekijken</p>
+					<a href="https://github.com/samjoosten" >Github</a>
+                    <a style="margin-left: 3%" href="https://drive.google.com/drive/folders/1TZaMbMh_Q55uYP4cc1V0uRmj64xFtnTE">Drive</a>
+				</div>
+			</div>
+		</div>
+
+	</div>
+	<!-- //Work -->
+	<!-- Modal1 -->
+	<div class="modal fade" id="myModal1" tabindex="-1" role="dialog">
+		<div class="modal-dialog">
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h5>Font and branding design for Printing company.</h5>
+					<p>Ut in ligula sollicitudin, auctor elit vel, mollis tortor. Nullam id magna in eros mollis porttitor vel et eros.Phasellus
+						sed iaculis nibh, non suscipit tortor. Aenean ante massa, lobortis et dolor eget, sollicitudin luctus arcu.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //Modal1 -->
+	<!-- Modal2 -->
+	<div class="modal fade" id="myModal2" tabindex="-1" role="dialog">
+		<div class="modal-dialog">
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<img src="images/w2-pop.jpg" alt=" " class="img-responsive">
+					<h5>Accessory design and corporate style for Elite Stationery Shop.</h5>
+					<p>Ut in ligula sollicitudin, auctor elit vel, mollis tortor. Nullam id magna in eros mollis porttitor vel et eros.Phasellus
+						sed iaculis nibh, non suscipit tortor. Aenean ante massa, lobortis et dolor eget, sollicitudin luctus arcu.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //Modal2 -->
+	<!-- Modal3 -->
+	<div class="modal fade" id="myModal3" tabindex="-1" role="dialog">
+		<div class="modal-dialog">
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<img src="images/w3.jpg" alt=" " class="img-responsive">
+					<h5>Font and branding design for Printing company.</h5>
+					<p>Ut in ligula sollicitudin, auctor elit vel, mollis tortor. Nullam id magna in eros mollis porttitor vel et eros.Phasellus
+						sed iaculis nibh, non suscipit tortor. Aenean ante massa, lobortis et dolor eget, sollicitudin luctus arcu.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //Modal3 -->
+	<!-- Modal4 -->
+	<div class="modal fade" id="myModal4" tabindex="-1" role="dialog">
+		<div class="modal-dialog">
+			<!-- Modal content-->
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<img src="images/w4.jpg" alt=" " class="img-responsive">
+					<h5>Accessory design and corporate style for Elite Stationery Shop.</h5>
+					<p>Ut in ligula sollicitudin, auctor elit vel, mollis tortor. Nullam id magna in eros mollis porttitor vel et eros.Phasellus
+						sed iaculis nibh, non suscipit tortor. Aenean ante massa, lobortis et dolor eget, sollicitudin luctus arcu.</p>
+				</div>d
+			</div>
+		</div>
+	</div>
+	<!-- //Modal4 -->
+
+	<!-- Footer -->
+	<div class="copyright">
+		<div class="container">
+			<p>&copy; 2017 Succinct . All Rights Reserved | Design by <a href="http://w3layouts.com/"> W3layouts </a></p>
+		</div>
+	</div>
+	<!-- //Footer -->
+	<!-- Move-top arrow -->
+	<a href="#home" class="scroll" id="toTop" style="display: block; position: center"> <span id="toTopHover" style="opacity: 1; margin: auto"> </span></a>
+	<!-- // Move-top arrow -->
+	<!--  Scripts -->
+	<script type='text/javascript' src='js/jquery-2.2.3.min.js'></script>
+	<!-- stats -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<script src="js/jquery.countup.js"></script>
+	<script>
+		$('.counter').countUp();
+	</script>
+	<!-- //stats -->
+
+
+	<!-- start-smoth-scrolling -->
+
+	<script type="text/javascript" src="js/move-top.js"></script>
+	<script type="text/javascript" src="js/easing.js"></script>
+	<script type="text/javascript">
+		jQuery(document).ready(function ($) {
+			$(".scroll").click(function (event) {
+				event.preventDefault();
+				$('html,body').animate({
+					scrollTop: $(this.hash).offset().top
+				}, 1000);
+			});
+		});
+	</script>
+	<!-- start-smoth-scrolling -->
+
+
+	<!--responsive-tabs-script-->
+	<script src="js/easy-responsive-tabs.js"></script>
+	<script>
+		$(document).ready(function () {
+			$('#horizontalTab').easyResponsiveTabs({
+				type: 'default', //Types: default, vertical, accordion           
+				width: 'auto', //auto or any width like 600px
+				fit: true, // 100% fit in a container
+				closed: 'accordion', // Start closed if in accordion view
+				activate: function (event) { // Callback function if tab is switched
+					var $tab = $(this);
+					var $info = $('#tabInfo');
+					var $name = $('span', $info);
+					$name.text($tab.text());
+					$info.show();
+				}
+			});
+			$('#verticalTab').easyResponsiveTabs({
+				type: 'vertical',
+				width: 'auto',
+				fit: true
+			});
+		});
+	</script>
+	<!--//responsive-tabs-script-->
+	<!-- here stars scrolling icon -->
+	<script type="text/javascript">
+		$(document).ready(function () {
+			/*
+				var defaults = {
+				containerID: 'toTop', // fading element id
+				containerHoverID: 'toTopHover', // fading element hover id
+				scrollSpeed: 1200,
+				easingType: 'linear' 
+				};
+			*/
+
+			$().UItoTop({
+				easingType: 'easeOutQuart'
+			});
+
+		});
+	</script>
+	<!-- //here ends scrolling icon -->
+	<!--js for bootstrap working-->
+	<script src="js/bootstrap.js"></script>
+	<!-- //for bootstrap working -->
+	<!-- // Scripts -->
+</body>
+
+</html>
